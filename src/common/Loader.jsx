d08ca53e2f71c25efe76c238a02aa9aa7cb6a390
sangharsh <sangharshdev69@gmail.com>
2026-04-28 01:51:28 +0530

    fix: disable background scrolling when loader is active

--- src/common/Loader.jsx
+++ src/common/Loader.jsx
@@ -1,9 +1,27 @@
 import { useSelector } from "react-redux";
 import ScaleLoader from "react-spinners/ScaleLoader";
 import "./Loader.css"
+import { useEffect } from "react";
 
 const Loader = () => {
   const loading = useSelector((state) => state.getJournalLoader);
+
+useEffect(() => {
+  if (loading) {
+    document.body.style.overflow = "hidden";
+    document.body.style.position = "fixed";
+    document.body.style.width = "100%";
+  } else {
+    document.body.style.overflow = "auto";
+    document.body.style.position = "static";
+  }
+
+  return () => {
+    document.body.style.overflow = "auto";
+    document.body.style.position = "static";
+  };
+}, [loading]);
+
 
   if (!loading) return null;
 
